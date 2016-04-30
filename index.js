a734627c795b29140cc6fb2475beb0174be6c109
hagbarth <emil@hivebeat.com>
2016-04-30 16:38:48 +0200

    add index and first function

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = function (aws) {
+  function sendMail (payload) {
+    const params = {
+        FunctionName: 'lambda_mail-production-1-0-0',
+        InvocationType: 'Event',
+        Payload: JSON.stringify(payload)
+      }
+    const lambda = new AWS.Lambda({
+        apiVersion: '2015-03-31'
+      })
+    return new Promise((resolve, reject) => {
+        lambda.invoke(params, function (err, data) {
+              if (err) {
+                      reject(err)
+                    }
+              resolve(data)
+            })
+      })
+  }
+}
+
