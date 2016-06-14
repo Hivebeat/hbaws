8b7ef66f20252a8cc80757c128ff0a3ffde3c285
hagbarth <emil@hivebeat.com>
2016-06-14 18:59:09 +0200

    add generalt lambda caller

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-module.exports = function (AWS) {
-  function sendMail (payload) {
+module.exports = function(AWS) {
+  function sendMail(payload) {
     const params = {
-        FunctionName: 'lambda_mail-production-1-0-0',
-        InvocationType: 'Event',
-        Payload: JSON.stringify(payload)
-      }
+      FunctionName: 'lambda_mail-production-1-0-0',
+      InvocationType: 'Event',
+      Payload: JSON.stringify(payload)
+    }
     const lambda = new AWS.Lambda({
-        apiVersion: '2015-03-31'
-      })
+      apiVersion: '2015-03-31'
+    })
     return new Promise((resolve, reject) => {
-        lambda.invoke(params, function (err, data) {
-              if (err) {
-                      reject(err)
-                    }
-              resolve(data)
-            })
+      lambda.invoke(params, function(err, data) {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(data)
       })
+    })
   }
+
+  function lambda(name, payload) {
+    const params = {
+      FunctionName: name,
+      InvocationType: 'Event',
+      Payload: JSON.stringify(payload)
+    }
+    const lambda = new AWS.Lambda({
+      apiVersion: '2015-03-31'
+    })
+    return new Promise((resolve, reject) => {
+      lambda.invoke(params, function(err, data) {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(data)
+      })
+    })
+  }
+
+
   return {
-    sendMail: sendMail
+      sendMail: sendMail
   }
 }
-
